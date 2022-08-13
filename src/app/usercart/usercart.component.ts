77a354f74bb9b21b56885e5cc111d193cb30bd8f
Vikram Chiluka <vikramchiluka18@gmail.com>
2022-08-13 20:34:21 +0530

    fixed

--- src/app/usercart/usercart.component.ts
+++ src/app/usercart/usercart.component.ts
@@ -29,7 +29,6 @@ export class UsercartComponent implements OnInit {
               this.count.push(1);
               
             }
-            console.log(this.products)
         }
       },
       err=>{
@@ -44,7 +43,6 @@ export class UsercartComponent implements OnInit {
   OnClicked(ind){
 
     let username=localStorage.getItem("username");
-    //console.log(this.products[ind]);
     let details=username+'-'+ind;
     this.userService.deleteProductFromCart(details).subscribe(res=>{
       if(res["message"]==='Cart-empty'){
@@ -53,35 +51,23 @@ export class UsercartComponent implements OnInit {
       else{
         this.products=res["data"]  
         alert(res["message"])
-        this.userService.updateDataObservable(res["data"])
-
-          // alert("Product deleted")        
+        this.userService.updateDataObservable(res["data"])     
       }
     },err=>{
       console.log(err);
     });
   }
   getTotal() {
-    
-    this.value=0
-    for(let x in this.products){
-      // console.log(x)
-      this.value += (this.products[x]["price"]*this.count[x]);
-      // console.log(this.products[x]["quantity"])
-    }
-  }
-
-  sendToPurchase(){
-    let username=localStorage.getItem("username");
     for(let i=0;i<this.products.length;i++){
       this.products["quantity"]=this.count[i];
     }
-    // let v=
-    this.userService.sendPurchase(this.products).subscribe(res=>{
-      alert(res["message"])
-    })
-    // console.log(this.products)
+    this.value=0
+    for (let x in this.products) {
+      this.value += (this.products[x]["price"]*this.count[x]);
+    }
+    
   }
+
   sendToTransaction(){
     let username=localStorage.getItem("username");
     let v={username:username,price:this.value,status:"on progress",purchased:this.products};
@@ -97,9 +83,20 @@ export class UsercartComponent implements OnInit {
       alert("Error");
     })   
     
-    this.sendToPurchase()
+    // this.sendToPurchase()
 
   }
+
+  sendToPurchase(){
+    for(let i=0;i<this.products.length;i++){
+      this.products[i]["quantity"]=this.count[i];
+    }
+    this.userService.sendPurchase(this.products).subscribe(res=>{
+      alert(res["message"])
+    })
+    this.sendToTransaction()
+  }
+  
   
   increment(ind) {
     this.count[ind]++;
