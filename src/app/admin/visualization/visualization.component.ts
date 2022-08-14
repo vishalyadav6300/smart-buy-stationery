606a66308258b2fc282983343a4172f8e95a8e3b
keerthana52 <purumandlakeerthana@gmail.com>
2022-08-14 13:45:12 +0530

    final pie

--- src/app/admin/visualization/visualization.component.ts
+++ src/app/admin/visualization/visualization.component.ts
@@ -16,8 +16,11 @@ export class VisualizationComponent implements OnInit {
     amountData = [];
     quantityData = [];
     data = [];
-    visdata=[];
-  labelsData = [];
+    quantitytop6=[];
+    quantitybottom6=[];
+  quantitylabeltop6 = [];
+  quantitylabelbottom6=[];
+
     
   constructor(private as:AdminService) { }
 
@@ -30,11 +33,13 @@ export class VisualizationComponent implements OnInit {
           //this.data=this.amountData
           //console.log(this.amountData[0]['quantity'])
           for (let x = 0; x < Math.min(this.amountData.length, 6);x++) {
-            this.labelsData.push( this.amountData[x]['productname'])
-            this.visdata.push(this.amountData[x]['quantity'])
+            this.quantitylabeltop6.push( this.amountData[x]['productname'])
+            this.quantitytop6.push(this.amountData[x]['quantity'])
+            this.quantitylabelbottom6.push(this.amountData[this.amountData.length-1-x]['productname'])
+            this.quantitybottom6.push(this.amountData[this.amountData.length-1-x]['quantity'])
             
           }
-          console.log(this.visdata);
+         
           
         }
       },error => {
@@ -90,24 +95,81 @@ error=>{
                 y: {
                     title: {
                       display: true,
-                      text: 'amount'
+                      text: 'quantity'
                     }
                   },
                   x: {
                     title: {
                       display: true,
-                      text: 'price'
+                      text: 'products'
                     }
                   }
             }
       }
         
     });
-    console.log(this.labelsData)
+    console.log(this.quantitylabeltop6)
 
-     await asyncFunction(myChart,this.labelsData,this.visdata)
+     await asyncFunction(myChart,this.quantitylabeltop6,this.quantitytop6)
+
+      this.piechartbottom6()
 
   }
+    async piechartbottom6() {
+
+        Chart.register(...registerables);
+  
+        let myChart = new Chart("myChartbottom6", {
+            type: 'pie',
+            data: {
+              
+                labels: [],
+                datasets: [{
+                    label: '',
+                    data: [],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        title: {
+                          display: true,
+                          text: 'quantity'
+                        }
+                      },
+                      x: {
+                        title: {
+                          display: true,
+                          text: 'products'
+                        }
+                      }
+                }
+          }
+            
+        });
+        //console.log(this.quantitylabeltop6)
+    
+         await asyncFunction(myChart,this.quantitylabelbottom6,this.quantitybottom6)
+
+
+            }
   
 
 }
@@ -123,5 +185,7 @@ async function asyncFunction(my,la,va){
    }
    my.update()
  }, 1000); // 2 seconds timeout
+
+
 }
 
